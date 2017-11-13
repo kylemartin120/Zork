a1249122c9b741570d9f802d38ab2ca7e1490a57
Kyle Martin <kmart120@hotmail.com>
2017-11-13 12:23:18 -0500

    first pass at Item

---
+++ include/Item.h
@@ -0,0 +1,23 @@
+#ifndef ITEM_H_
+#define ITEM_H_
+
+#include "GameObject.h"
+#include "Trigger.h"
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+class Item : public GameObject {
+ private:
+  string writing;
+  vector<string> onCommands;
+
+ public:
+  Item(string n, string s, string d, string w, vector<string> on,
+       vector<Trigger*> t);
+  virtual ~Item();
+  void printWriting();
+  vector<string> getOnCommands();
+};
+#endif
---
+++ source/Item.cpp
@@ -0,0 +1,23 @@
+#include "Item.h"
+#include "GameObject.h"
+#include "Trigger.h"
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+Item::Item(string n, string s, string d, string w, vector<string> on,
+	   vector<Trigger*> t) : GameObject(n, d, s, t) {
+  writing = w;
+  onCommands = on;
+}
+
+Item::~Item() {}
+
+void Item::printWriting() {
+  cout << writing << endl;
+}
+
+vector<string> getOnCommands() {
+  return onCommands;
+}
